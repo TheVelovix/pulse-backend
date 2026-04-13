1c58cdcc881ec761195a45f3b2e3fe0363a923fe
Melos Shabi <melosshabi@protonmail.com>
2026-04-13 09:02:49 +0200

    Fixed CORS

--- Program.cs
+++ Program.cs
@@ -80,8 +80,7 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
         .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
+        .AllowAnyMethod();
     });
 });
 
